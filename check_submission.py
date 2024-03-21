ab5a1460d108e43e1063cb9be164f7fd36692199
Shrikant Arvavasu <54639052+ashrikant39@users.noreply.github.com>
2024-03-21 00:28:10 -0400

    Update check_submission.py

--- check_submission.py
+++ check_submission.py
@@ -96,7 +96,7 @@ if __name__ == "__main__":
         ["filters.py", "corners.py", "blob_detection.py", "common.py"],  # hw2
         ["common.py", "homography.py", "task5.py", "task6.py", "mypanorama1.jpg", "mypanorama2.jpg"], # hw3
         ["layers.py", "fitting.py", "functions.py", "train.py", "two_layer_net.py"],  # hw4
-        ["part1.ipynb", "part2.ipynb", "part3.ipynb"],  # hw5
+        ["simple_diffusion.py", "condition_methods.py", "hw5_gan.ipynb", "hw5_diffusion.ipynb"],  # hw5
         ["task1.py", "task23.py"], # hw6
     ]
         
